bf76420433eec1edfb6e09d597801fb7967ff6bf
nithil17 <nithilnayan@yahoo.com>
2026-06-05 20:26:34 +0530

    added userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
+const jwt = require("jsonwebtoken");
+
 router.post("/register", async (req, res) => {
     try {
 
@@ -37,6 +39,51 @@ router.post("/register", async (req, res) => {
 
         res.status(201).json({
             message: "User registered successfully"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1d"
+            }
+        );
+
+        res.status(200).json({
+            token
         });
 
     } catch (error) {
